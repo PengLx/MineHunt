22e7ca4cf6da539ea49cba51782a79c48aca46d8
PengLx <35966554+PengLx@users.noreply.github.com>
2021-01-10 11:55:37 +0800

    Update GameProgressManager.java

--- src/main/java/net/mcxk/minehunt/game/GameProgressManager.java
+++ src/main/java/net/mcxk/minehunt/game/GameProgressManager.java
@@ -32,31 +32,17 @@ public class GameProgressManager {
         switch (progress){
             case NOT_STARTED:
             case GAME_STARTING:
-                plugin.getGame().getInGamePlayers().forEach(player -> player.getInventory().addItem(new ItemStack(Material.BREAD,5)));
-                break;
             case STONE_AGE:
             case IRON_MINED:
-                broadcastProgress(progress,true,true);
-                plugin.getGame().getInGamePlayers().forEach(player -> player.getInventory().addItem(new ItemStack(Material.IRON_ORE,8)));
-                plugin.getGame().getInGamePlayers().forEach(player -> player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION,600,1)));
                 break;
             case COMPASS_UNLOCKED:
             case ENTER_NETHER:
-                broadcastProgress(progress,true,false);
-                plugin.getGame().getInGamePlayers().forEach(player -> player.getInventory().addItem(new ItemStack(Material.OBSIDIAN,4)));
-                plugin.getGame().getInGamePlayers().forEach(player -> player.getInventory().addItem(new ItemStack(Material.FLINT,1)));
                 break;
             case GET_BLAZE_ROD:
-                broadcastProgress(progress,false,true);
-                plugin.getGame().getInGamePlayers().forEach(player -> player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE,300,1)));
                 break;
             case GET_ENDER_PERAL:
-                broadcastProgress(progress,true,false);
-                plugin.getGame().getInGamePlayers().forEach(player -> player.getInventory().addItem(new ItemStack(Material.ENDER_PEARL,1)));
                 break;
             case ENTER_END:
-                broadcastProgress(progress,true,false);
-                plugin.getGame().getInGamePlayers().forEach(player -> player.getInventory().addItem(new ItemStack(Material.WATER_BUCKET,1)));
                 break;
             case KILLED_DRAGON:
                 broadcastProgress(progress,false,false);
